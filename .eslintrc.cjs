06f3a9d498feae3feefcccc22221d1f69c9c1d34
HERNAN Ismael Norberto Jesús <ismael29h@gmail.com>
2023-06-16 16:02:42 -0300

    ESLint/Prettier instalado y configurado

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,15 +1,25 @@
 module.exports = {
-  env: { browser: true, es2020: true },
-  extends: [
-    'eslint:recommended',
-    'plugin:react/recommended',
-    'plugin:react/jsx-runtime',
-    'plugin:react-hooks/recommended',
-  ],
-  parserOptions: { ecmaVersion: 'latest', sourceType: 'module' },
-  settings: { react: { version: '18.2' } },
-  plugins: ['react-refresh'],
-  rules: {
-    'react-refresh/only-export-components': 'warn',
-  },
-}
+    env: {
+        browser: true,
+        es2021: true,
+    },
+    extends: [
+        'standard',
+        'plugin:react/recommended',
+        'plugin:react/jsx-runtime',
+        'eslint-config-prettier',
+    ],
+    settings: {
+        react: {
+            version: 'detect',
+        },
+    },
+    parserOptions: {
+        ecmaVersion: 'latest',
+        sourceType: 'module',
+    },
+    plugins: ['react'],
+    rules: {
+        'no-unused-vars': 'warn',
+    },
+};
