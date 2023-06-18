46c97acae864f9654850452a38b3a352c83564fe
HERNAN Ismael Norberto Jesús <ismael29h@gmail.com>
2023-06-18 18:15:27 -0300

    Decisión: Estructura de carpetas del proyecto

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App.jsx';
-import './index.css';
+import Root from './routes/Root.jsx';
+import './styles/index.css';
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+
+const router = createBrowserRouter([
+    {
+        path: '/',
+        element: <Root />,
+    },
+]);
+
+ReactDOM.createRoot(document.getElementById('main')).render(
     <React.StrictMode>
-        <App />
+        <RouterProvider router={router} />
     </React.StrictMode>
 );
